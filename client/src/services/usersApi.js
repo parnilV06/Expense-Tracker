be81eb66e157c00ecf1c279e1e640580147f2867
parnil <06v.parnil@gmail.com>
2026-05-29 11:59:58 +0530

    minor bug fixes

--- client/src/services/usersApi.js
+++ client/src/services/usersApi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || ''
+const API_BASE_URL = (import.meta.env.VITE_API_URL || '').replace(/\/$/, '')
 
 async function requestUser(path, payload) {
 	const response = await fetch(`${API_BASE_URL}/api/users/${path}`, {
